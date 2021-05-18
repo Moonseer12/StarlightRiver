ab6a8e51a5dbaecc16f140e54b5134210ecbc3c1
Oliver Heamon <oliverheamon@gmail.com>
2021-05-18 23:06:14 +0100

    Made message not bad

--- Important/push.bat
+++ Important/push.bat
@@ -4,5 +4,5 @@ echo Pushing all local changes to the 1.4 branch.
 set /p message="Commit Message: "
 
 git add .
-git commit -a -m "Pushed automatically on %date%: %message%"
+git commit -a -m "%message%"
 git push origin 1.4
