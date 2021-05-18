c5348bad002f66d34ba3d06359adb31296f3e21f
Oliver Heamon <oliverheamon@gmail.com>
2021-05-18 22:42:46 +0100

    removed background test until further notice

--- StarlightRiver.cs
+++ StarlightRiver.cs
@@ -1,8 +1,18 @@
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace StarlightRiver
 {
     public class StarlightRiver : Mod
     {
+    }
+
+    public class SLRMenu : ModMenu
+    {
+        public override Asset<Texture2D> Logo => ModContent.GetTexture("StarlightRiver/amogus");
+
+        public override int Music => MusicID.WindyDay;
     }
 }
